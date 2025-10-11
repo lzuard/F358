5a511da28061912563ef208d57659bc9ee951552
lzuard <sasha.20133@gmail.com>
2025-10-11 18:40:38 +0300

    [ClientApp] Delete avalonia client app

--- F358.Client/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace F358.Client.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    //[ObservableProperty]
-    private string Greeting => "Welcome to Avalonia!";
-}
